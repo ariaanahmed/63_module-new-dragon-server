e4c7df75f6403f3d5df5a980edcbdc79c00d0f22
ariaan ahmed <you@example.com>
2023-04-30 10:57:12 +0600

    / missing problem solved

--- index.js
+++ index.js
@@ -5,15 +5,42 @@ const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('news dragon ka fragon is running')
 })
 
-app.use(cors());
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+// all news load from api
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+// specific vabe ekta news ui te dekhanor system
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find((n) => n._id === id);
+    res.send(selectedNews)
+})
+
+// category wise 
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    
+    if (id === 0) {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+
 })
 
 app.listen(port, () => {
